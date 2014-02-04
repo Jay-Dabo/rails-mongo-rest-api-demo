d5415efc6ac46935be851f8f0f4eac354b47a8d5
Alex Emelyanov <aemelyanov@viacode.com>
2014-02-04 18:33:45 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsMongoRestApi::Application.config.secret_key_base = '8ce35978f0789437734fe917c42a59ccd29198342f7d8c342b4bf7b52470df94b34468560686d8cc70d1e6645c9751740d6abb76a4726c2be0902e0721dafd57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsMongoRestApi::Application.config.session_store :cookie_store, key: '_rails-mongo-rest-api_session'
